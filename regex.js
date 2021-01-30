7affc2533fef1b2a3dd2797603c5cc3dce7cd239
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-29 15:27:19 -1000

    matching literal strings with regex

--- regex.js
+++ regex.js
@@ -1,4 +1,9 @@
 // Regex: using the test method
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString); // Change this line
+let result = myRegex.test(myString);
+
+// Regex: match literal strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
