49e6e82dc8d8cc107d22f89b72e4cd822871aeaa
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-29 15:29:36 -1000

    Match literal strings with different possibilities

--- regex.js
+++ regex.js
@@ -7,3 +7,10 @@ let result = myRegex.test(myString);
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/;
 let result = waldoRegex.test(waldoIsHiding);
+
+// Match a literal string with different possiblities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
+
+//
