afb052e714fc05b494c85157f1488750d651792d
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-01-29 15:31:07 -1000

    ignoring case while matching

--- regex.js
+++ regex.js
@@ -13,4 +13,7 @@ let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
 
-//
+// ignore case while matching
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i;
+let result = fccRegex.test(myString);
